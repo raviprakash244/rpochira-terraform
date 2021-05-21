eae2796d090cc664e0232b1eaba3c2f884b62bf5
Raviprakash Pochiraju <raviprakashpochiraju@Raviprakashs-MacBook-Pro.local>
2021-05-22 01:46:29 +0530

    Learning Terraforms

--- variables.tf
+++ variables.tf
@@ -1,24 +1,17 @@
 
 variable "vpc_region" {
   description = "Region in which VPC needs to be created"
-  default = "us-east-1a"
 }
 
 variable "availability_zone" {
   description = "availability zone used. , based on region"
-  default = {
-    us-east-1 = "us-east-1a"
-    us-west-1 = "us-west-1a"
-  }
 }
 
 variable "vpc_name" {
   description = "VPC for building demos"
-  default = "vpc_db"
 }
 
 
 variable "vpc_cidr_block" {
   description = "Uber IP addressing for demo Network"
-  default = "10.10.0.0/16"
 }
