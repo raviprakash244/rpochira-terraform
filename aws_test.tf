61fa38482d228e4c62f6c20ec6ceaddb5b4b2715
Raviprakash Pochiraju <raviprakashpochiraju@Raviprakashs-MacBook-Pro.local>
2021-06-04 18:27:00 +0530

    Aurora cluster

--- aws_test.tf
+++ aws_test.tf
@@ -1,16 +1,16 @@
 # Setup our aws provider
 
-provider "aws" {
-  region = "${var.vpc_region}"
-}
+#provider "aws" {
+#  region = "${var.vpc_region}"
+#}
 
 # Define a vpc
-resource "aws_vpc" "${var.vpc_name}" {
-  cidr_block = "${var.vpc_cidr_block}"
-  tags = {
-    Name = "${var.vpc_name}"
-  }
-}
+#resource "aws_vpc" "${var.vpc_name}" {
+#  cidr_block = "${var.vpc_cidr_block}"
+#  tags = {
+#    Name = "${var.vpc_name}"
+#  }
+#}
 
 
 resource "aws_rds_cluster" "postgresql" {
