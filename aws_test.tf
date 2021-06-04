4d2e80848bd467d3b971a70a3bbc90cca8e373bd
Raviprakash Pochiraju <raviprakashpochiraju@Raviprakashs-MacBook-Pro.local>
2021-06-04 18:28:39 +0530

    Aurora cluster

--- aws_test.tf
+++ aws_test.tf
@@ -16,7 +16,8 @@
 resource "aws_rds_cluster" "postgresql" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-postgresql"
-  availability_zones      = [ var.vpc_region ]
+  region                  = "us-east-1"
+  availability_zones      = [ "us-east-1a" ]
   database_name           = "mydb"
   master_username         = "rpochira"
   master_password         = "rpochira"
