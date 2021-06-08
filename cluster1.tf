f706b65b8d12b5a46391c4f7ac973b4d12100d1e
raviprakash244 <36137174+raviprakash244@users.noreply.github.com>
2021-06-08 18:12:07 +0530

    Update cluster1.tf

--- cluster1.tf
+++ cluster1.tf
@@ -1,9 +1,10 @@
 # Setup our aws provider
 
-provider "aws" {}
+provider "aws" {
+  region = "us-east-1"
+}
 
 
 resource "aws_rds_cluster" "aurora-cluster-demo2" {
   preferred_backup_window = "10:00-09:00"
 }
-
