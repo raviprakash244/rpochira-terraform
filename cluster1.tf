9c324cc4a536d6138ae30bd75e1a7ddc208256ae
raviprakash244 <36137174+raviprakash244@users.noreply.github.com>
2021-06-08 17:51:50 +0530

    Update cluster1.tf

--- cluster1.tf
+++ cluster1.tf
@@ -17,15 +17,3 @@ resource "aws_rds_cluster" "aurora-cluster-demo2" {
   skip_final_snapshot     = "true"
 }
 
-
-resource "aws_rds_cluster" "aurora-cluster-demo1" {
-  cluster_identifier      = "aurora-cluster-demo1"
-  engine                  = "aurora-postgresql"
-  availability_zones      = [ "us-east-1a" ]
-  database_name           = "mydb"
-  master_username         = "rpochira"
-  master_password         = "rpochira"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot     = "true"
-}
