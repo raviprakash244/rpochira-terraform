f10eec3a4e9b3844485dcbadaaa590d439cd5cf4
raviprakash244 <36137174+raviprakash244@users.noreply.github.com>
2021-06-08 17:22:56 +0530

    Create cluster1.tf

---
+++ cluster1.tf
@@ -0,0 +1,19 @@
+# Setup our aws provider
+
+provider "aws" {
+  alias  = "aurora-cluster-demo1"
+  region = "${var.vpc_region}"
+}
+
+
+resource "aws_rds_cluster" "aurora-cluster-demo1" {
+  cluster_identifier      = "aurora-cluster-demo1"
+  engine                  = "aurora-postgresql"
+  availability_zones      = [ "us-east-1a" ]
+  database_name           = "mydb"
+  master_username         = "rpochira"
+  master_password         = "rpochira"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = "true"
+}
