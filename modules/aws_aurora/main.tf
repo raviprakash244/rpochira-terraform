f22550386730e2ebdc32d620aabcf7c464f9f0a3
raviprakash244 <36137174+raviprakash244@users.noreply.github.com>
2021-06-11 11:40:28 +0530

    Update main.tf

--- modules/aws_aurora/main.tf
+++ modules/aws_aurora/main.tf
@@ -1,13 +1,6 @@
 # Setup our aws provider
 resource "aws_rds_cluster" "aws_aurora" {
-  cluster_identifier      = var.cluster_name
-  engine                  = "aurora-postgresql"
-  availability_zones      = [ "us-east-1a" ]
-  database_name           = "mydb"
-  master_username         = "rpochira"
-  master_password         = "rpochira"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
+  preferred_backup_window = "08:00-09:00"
   skip_final_snapshot     = "true"
   deletion_protection     = "true"
 }
