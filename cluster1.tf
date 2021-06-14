24d1034209608fbace9e4bbfd0de5dd960b54953
raviprakash244 <36137174+raviprakash244@users.noreply.github.com>
2021-06-14 15:17:20 +0530

    Update cluster1.tf

--- cluster1.tf
+++ cluster1.tf
@@ -1,35 +1,23 @@
-module "postgresql_rds" {
-    source = "github.com/azavea/terraform-aws-postgresql-rds"
-    vpc_id = "vpc-4ed74233"
-    allocated_storage = "32"
-    engine_version = "9.4.4"
-    instance_type = "db.t2.micro"
-    storage_type = "gp2"
-    database_identifier = "rpochira"
-    database_name = "rpochira"
-    database_username = "rpochira"
-    database_password = "rpochira"
-    database_port = "5432"
-    backup_retention_period = "30"
-    backup_window = "04:00-04:30"
-    maintenance_window = "sun:04:30-sun:05:30"
-    auto_minor_version_upgrade = false
-    multi_availability_zone = true
-    storage_encrypted = false
-    subnet_group = "default"
-    parameter_group = "default.aurora-postgresql11"
-    monitoring_interval = "60"
-    deletion_protection = true
-    cloudwatch_logs_exports = ["postgresql"]
+# Setup our aws provider
+terraform {
+  backend "remote" {
+      hostname = "app.terraform.io"
+      organization = "rpochira"
+      workspaces {
+        name = "rpochira"
+      }
+  }
+}
 
-    alarm_cpu_threshold = "75"
-    alarm_disk_queue_threshold = "10"
-    alarm_free_disk_threshold = "5000000000"
-    alarm_free_memory_threshold = "128000000"
-    alarm_actions = ["arn:aws:sns..."]
-    ok_actions = ["arn:aws:sns..."]
-    insufficient_data_actions = ["arn:aws:sns..."]
+resource "aws_rds_cluster" "aurora-cluster-demo" {
+  cluster_identifier      = "aurora-cluster-demo"
+  engine                  = "aurora-postgresql"
+  availability_zones      = [ "us-east-1a" ]
+  database_name           = "mydb"
+  master_username         = "rpochira"
+  master_password         = "rpochira"
+  backup_retention_period = 5
+  preferred_backup_window = "08:00-09:00"
+  db_subnet_group_name    = "default"
 
-    project = "Something"
-    environment = "Staging"
 }
