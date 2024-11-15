9ec1d6c64c7d62dc92cd481ee79f2a84537c48dd
Ravipraksh Pochiraju <raviprakash244@gmail.com>
2024-11-16 00:15:36 +0530

    Testing terraform code

--- cluster1.tf
+++ cluster1.tf
@@ -1,152 +1,161 @@
-# Setup AWS provider with remote backend
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "rpochira"
-    workspaces {
-      name = "rpochira"
-    }
-  }
-}
+# # Setup AWS provider with remote backend
+# terraform {
+#   backend "remote" {
+#     hostname = "app.terraform.io"
+#     organization = "rpochira"
+#     workspaces {
+#       name = "rpochira"
+#     }
+#   }
+# }
 
-# Define the subnet names list as a variable
-variable "subnet_name_list" {
-  type    = list(string)
-  default = ["private-subnet-1", "private-subnet-2", "private-subnet-3"]
-}
+# # Define the subnet names list as a variable
+# variable "subnet_name_list" {
+#   type    = list(string)
+#   default = ["private-subnet-1", "private-subnet-2", "private-subnet-3"]
+# }
 
-# Launch Template for EC2 instances
-resource "aws_launch_template" "example" {
-  name          = "couchbase-data-launch-template"
-  image_id      = "ami-030c239b5d3296394"  # Replace with your AMI ID
-  instance_type = "t2.micro"                # Replace with your instance type
+# resource "aws_network_interface" "eni" {
+#   count         = length(var.subnet_name_list)
+#   subnet_id     = data.aws_subnet.subnets[count.index].id
+#   security_groups = ["sg-057749862a8753300"] # Replace with appropriate security group ID
+#   description   = "ENI for Couchbase"
+# }
 
-  key_name      = "terminal"               # Replace with your SSH key
 
-  network_interfaces {
-    associate_public_ip_address = false
-    security_groups             = ["sg-057749862a8753300"]  # Replace with your security group ID
-  }
+# # Launch Template for EC2 instances
+# resource "aws_launch_template" "example" {
+#   name          = "couchbase-data-launch-template"
+#   image_id      = "ami-030c239b5d3296394"  # Replace with your AMI ID
+#   instance_type = "t2.micro"                # Replace with your instance type
 
-  user_data = base64encode(<<EOF
-#!/bin/bash
+#   key_name      = "terminal"               # Replace with your SSH key
 
-yum install -y aws-cli
-INSTANCE_ID=$$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-REGION="us-east-1"  # Correct region identifier
-TAG_NAME="data-$${INSTANCE_ID}"
+#   network_interfaces {
+#     associate_public_ip_address = false
+#     network_interface_id = aws_network_interface.eni[count.index % length(var.subnet_name_list)].id
+#     device_index         = 0
+#   }
 
-# Add tags to the instance
-aws ec2 create-tags \
-  --resources "$$INSTANCE_ID" \
-  --tags Key=UniqueName,Value="$$TAG_NAME" \
-  --region "$$REGION"
-EOF
-  )
-}
+#   user_data = base64encode(<<EOF
+# #!/bin/bash
 
-# Retrieve subnets with the specified tags
-data "aws_subnet" "subnets" { 
-  count = 3
-  filter { 
-    name   = "tag:Name"
-    values = ["*${var.subnet_name_list[(count.index % 3)]}*"]
-  }
-}
+# yum install -y aws-cli
+# INSTANCE_ID=$$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+# REGION="us-east-1"  # Correct region identifier
+# TAG_NAME="data-$${INSTANCE_ID}"
 
-# Local variable to hold subnet IDs
-locals {
-  subnet_ids = [for subnet in data.aws_subnet.subnets : subnet.id]
-}
+# # Add tags to the instance
+# aws ec2 create-tags \
+#   --resources "$$INSTANCE_ID" \
+#   --tags Key=UniqueName,Value="$$TAG_NAME" \
+#   --region "$$REGION"
+# EOF
+#   )
+# }
 
-# Auto Scaling Group resource
-resource "aws_autoscaling_group" "couchbase_data" {
-  desired_capacity     = 1
-  max_size             = 1
-  min_size             = 1
-  vpc_zone_identifier  = local.subnet_ids
+# # Retrieve subnets with the specified tags
+# data "aws_subnet" "subnets" { 
+#   count = 3
+#   filter { 
+#     name   = "tag:Name"
+#     values = ["*${var.subnet_name_list[(count.index % 3)]}*"]
+#   }
+# }
 
-  launch_template {
-    id      = aws_launch_template.example.id
-    version = "$Latest"
-  }
+# # Local variable to hold subnet IDs
+# locals {
+#   subnet_ids = [for subnet in data.aws_subnet.subnets : subnet.id]
+# }
 
-  tag {
-    key                 = "Name"
-    value               = "asg-example-instance"
-    propagate_at_launch = true
-  }
+# # Auto Scaling Group resource
+# resource "aws_autoscaling_group" "couchbase_data" {
+#   desired_capacity     = 1
+#   max_size             = 1
+#   min_size             = 1
+#   vpc_zone_identifier  = local.subnet_ids
 
-  lifecycle {
-    create_before_destroy = true
-  }
+#   launch_template {
+#     id      = aws_launch_template.example.id
+#     version = "$Latest"
+#   }
 
-  instance_refresh {
-    strategy = "Rolling"
-    preferences {
-      min_healthy_percentage = 100
-    }
-  }
+#   tag {
+#     key                 = "Name"
+#     value               = "asg-example-instance"
+#     propagate_at_launch = true
+#   }
 
-  health_check_type         = "EC2"
-  health_check_grace_period = 60
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
 
-# IAM Role for Auto Scaling Lifecycle Hook to access SNS
-resource "aws_iam_role" "example_lifecycle_role" {
-  name = "example-lifecycle-role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "autoscaling.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
+#   instance_refresh {
+#     strategy = "Rolling"
+#     preferences {
+#       min_healthy_percentage = 100
+#     }
+#   }
 
-# IAM Role Policy to allow SNS publishing
-resource "aws_iam_role_policy" "example_lifecycle_policy" {
-  role = aws_iam_role.example_lifecycle_role.name
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "sns:Publish",
-      "Resource": "arn:aws:sns:us-east-1:911167901101:asg_launch"
-    }
-  ]
-}
-EOF
-}
+#   health_check_type         = "EC2"
+#   health_check_grace_period = 60
+# }
 
-# Auto Scaling Lifecycle Hook to capture instance launch event
-resource "aws_autoscaling_lifecycle_hook" "example_hook" {
-  autoscaling_group_name = aws_autoscaling_group.couchbase_data.name
-  name                 = "example-lifecycle-hook"
-  lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
-  default_result       = "CONTINUE"
-  heartbeat_timeout    = 120
+# # IAM Role for Auto Scaling Lifecycle Hook to access SNS
+# resource "aws_iam_role" "example_lifecycle_role" {
+#   name = "example-lifecycle-role"
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": "sts:AssumeRole",
+#       "Principal": {
+#         "Service": "autoscaling.amazonaws.com"
+#       },
+#       "Effect": "Allow",
+#       "Sid": ""
+#     }
+#   ]
+# }
+# EOF
+# }
 
-  notification_target_arn = "arn:aws:sns:us-east-1:911167901101:asg_launch"
-  role_arn                = aws_iam_role.example_lifecycle_role.arn
-}
+# # IAM Role Policy to allow SNS publishing
+# resource "aws_iam_role_policy" "example_lifecycle_policy" {
+#   role = aws_iam_role.example_lifecycle_role.name
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Action": "sns:Publish",
+#       "Resource": "arn:aws:sns:us-east-1:911167901101:asg_launch"
+#     }
+#   ]
+# }
+# EOF
+# }
 
-# Output to display subnet IDs
-output "subnet_ids" {
-  description = "The list of subnet IDs"
-  value       = [for subnet in data.aws_subnet.subnets : subnet.id]
-}
+# # Auto Scaling Lifecycle Hook to capture instance launch event
+# resource "aws_autoscaling_lifecycle_hook" "example_hook" {
+#   autoscaling_group_name = aws_autoscaling_group.couchbase_data.name
+#   name                 = "example-lifecycle-hook"
+#   lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
+#   default_result       = "CONTINUE"
+#   heartbeat_timeout    = 120
 
-output "launch_template_version" {
-  value = aws_launch_template.example.latest_version
-}
+#   notification_target_arn = "arn:aws:sns:us-east-1:911167901101:asg_launch"
+#   role_arn                = aws_iam_role.example_lifecycle_role.arn
+# }
+
+# # Output to display subnet IDs
+# output "subnet_ids" {
+#   description = "The list of subnet IDs"
+#   value       = [for subnet in data.aws_subnet.subnets : subnet.id]
+# }
+
+# output "launch_template_version" {
+#   value = aws_launch_template.example.latest_version
+# }
