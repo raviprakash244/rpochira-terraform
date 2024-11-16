4e97b2e291ac320553a83d3780d53c13467d319d
Ravipraksh Pochiraju <raviprakash244@gmail.com>
2024-11-16 22:36:10 +0530

    Testing terraform code

--- cluster1.tf
+++ cluster1.tf
@@ -1,168 +1,168 @@
-# terraform {
-#   backend "remote" {
-#     hostname     = "app.terraform.io"
-#     organization = "rpochira"
-#     workspaces {
-#       name = "rpochira"
-#     }
-#   }
-# }
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "rpochira"
+    workspaces {
+      name = "rpochira"
+    }
+  }
+}
 
-# variable "instance_count" {
-#   type    = number
-#   default = 6
-# }
+variable "instance_count" {
+  type    = number
+  default = 3
+}
 
-# variable "ami_id" {
-#   type    = string
-#   default = "ami-030c239b5d3296394"
-# }
+variable "ami_id" {
+  type    = string
+  default = "ami-030c239b5d3296394"
+}
 
-# variable "security_group" {
-#   type    = string
-#   default = "sg-057749862a8753300"
-# }
+variable "security_group" {
+  type    = string
+  default = "sg-057749862a8753300"
+}
 
-# variable "instance_type" {
-#   type    = string
-#   default = "t2.micro"
-# }
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
 
-# variable "subnet_name_list" {
-#   type    = list(string)
-#   default = ["private-subnet-1", "private-subnet-2", "private-subnet-3"]
-# }
+variable "subnet_name_list" {
+  type    = list(string)
+  default = ["private-subnet-1", "private-subnet-2", "private-subnet-3"]
+}
 
-# data "aws_subnet" "subnets" { 
-#   for_each = toset(var.subnet_name_list)
-#   filter { 
-#     name   = "tag:Name"
-#     values = ["*${each.value}*"]
-#   }
-# }
+data "aws_subnet" "subnets" { 
+  for_each = toset(var.subnet_name_list)
+  filter { 
+    name   = "tag:Name"
+    values = ["*${each.value}*"]
+  }
+}
 
-# locals {
-#   all_subnet_ids = [for subnet in data.aws_subnet.subnets : subnet.id]
-#   subnet_index   = [for i in range(var.instance_count) : i % length(local.all_subnet_ids)]
-# }
+locals {
+  all_subnet_ids = [for subnet in data.aws_subnet.subnets : subnet.id]
+  subnet_index   = [for i in range(var.instance_count) : i % length(local.all_subnet_ids)]
+}
 
-# resource "aws_network_interface" "data_eni" {
-#   count             = var.instance_count
-#   subnet_id         = local.all_subnet_ids[local.subnet_index[count.index]]
-#   security_groups = [var.security_group]
-#   tags = {
-#     Subnet               = local.all_subnet_ids[local.subnet_index[count.index]]
-#     UniqueTag            = "data_${count.index}"
-#   }
-# }
+resource "aws_network_interface" "data_eni" {
+  count             = var.instance_count
+  subnet_id         = local.all_subnet_ids[local.subnet_index[count.index]]
+  security_groups = [var.security_group]
+  tags = {
+    Subnet               = local.all_subnet_ids[local.subnet_index[count.index]]
+    UniqueTag            = "data_${count.index}"
+  }
+}
 
-# resource "aws_launch_template" "couchbase_data" {
-#   name          = "couchbase-data-launch-template"
-#   image_id      = var.ami_id
-#   instance_type = var.instance_type
-#   key_name      = "terminal"
+resource "aws_launch_template" "couchbase_data" {
+  name          = "couchbase-data-launch-template"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+  key_name      = "terminal"
 
-#   network_interfaces {
-#     associate_public_ip_address = false
-#     security_groups             = [var.security_group]
-#   }
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [var.security_group]
+  }
 
-#   user_data = base64encode(<<EOF
-# #!/bin/bash
-# yum install -y aws-cli
-# INSTANCE_ID=$$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-# REGION="us-east-1"  # Correct region identifier
-# TAG_NAME="data-$${INSTANCE_ID}"
-# aws ec2 create-tags \
-#   --resources "$$INSTANCE_ID" \
-#   --tags Key=UniqueName,Value="$$TAG_NAME" \
-#   --region "$$REGION" >> /tmp/userdata.log 2>&1
-# EOF
-#   )
-# }
+  user_data = base64encode(<<EOF
+#!/bin/bash
+yum install -y aws-cli
+INSTANCE_ID=$$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+REGION="us-east-1"  # Correct region identifier
+TAG_NAME="data-$${INSTANCE_ID}"
+aws ec2 create-tags \
+  --resources "$$INSTANCE_ID" \
+  --tags Key=UniqueName,Value="$$TAG_NAME" \
+  --region "$$REGION" >> /tmp/userdata.log 2>&1
+EOF
+  )
+}
 
-# resource "aws_autoscaling_group" "couchbase_data" {
-#   desired_capacity    = var.instance_count
-#   max_size            = var.instance_count
-#   min_size            = var.instance_count
-#   vpc_zone_identifier = local.all_subnet_ids
+resource "aws_autoscaling_group" "couchbase_data" {
+  desired_capacity    = var.instance_count
+  max_size            = var.instance_count
+  min_size            = var.instance_count
+  vpc_zone_identifier = local.all_subnet_ids
 
-#   launch_template {
-#     id      = aws_launch_template.couchbase_data.id 
-#     version = "$Latest"
-#   }
+  launch_template {
+    id      = aws_launch_template.couchbase_data.id 
+    version = "$Latest"
+  }
 
-#   tag {
-#     key                 = "Name"
-#     value               = "asg-cb-data"
-#     propagate_at_launch = true
-#   }
+  tag {
+    key                 = "Name"
+    value               = "asg-cb-data"
+    propagate_at_launch = true
+  }
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
+  lifecycle {
+    create_before_destroy = true
+  }
 
-#   instance_refresh {
-#     strategy = "Rolling"
-#     preferences {
-#       min_healthy_percentage = 100
-#     }
-#   }
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 100
+    }
+  }
 
-#   health_check_type         = "EC2"
-#   health_check_grace_period = 60
-# }
+  health_check_type         = "EC2"
+  health_check_grace_period = 60
+}
 
-# resource "aws_iam_role" "example_lifecycle_role" {
-#   name = "example-lifecycle-role"
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "autoscaling.amazonaws.com"
-#       },
-#       "Effect": "Allow",
-#       "Sid": ""
-#     }
-#   ]
-# }
-# EOF
-# }
+resource "aws_iam_role" "example_lifecycle_role" {
+  name = "example-lifecycle-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "autoscaling.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
 
-# resource "aws_iam_role_policy" "example_lifecycle_policy" {
-#   role = aws_iam_role.example_lifecycle_role.name
-#   policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Action": "sns:Publish",
-#       "Resource": "arn:aws:sns:us-east-1:911167901101:asg_launch"
-#     }
-#   ]
-# }
-# EOF
-# }
+resource "aws_iam_role_policy" "example_lifecycle_policy" {
+  role = aws_iam_role.example_lifecycle_role.name
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sns:Publish",
+      "Resource": "arn:aws:sns:us-east-1:911167901101:asg_launch"
+    }
+  ]
+}
+EOF
+}
 
-# resource "aws_autoscaling_lifecycle_hook" "example_hook" {
-#   for_each = toset(local.all_subnet_ids)  # Iterate over subnets or ASGs
+resource "aws_autoscaling_lifecycle_hook" "example_hook" {
+  for_each = toset(local.all_subnet_ids)  # Iterate over subnets or ASGs
 
-#   autoscaling_group_name  = aws_autoscaling_group.couchbase_data.name  
-#   name                   = "asg-cbdata-lifecycle-hook"
-#   lifecycle_transition   = "autoscaling:EC2_INSTANCE_LAUNCHING"
-#   default_result         = "CONTINUE"
-#   heartbeat_timeout      = 120
+  autoscaling_group_name  = aws_autoscaling_group.couchbase_data.name  
+  name                   = "asg-cbdata-lifecycle-hook"
+  lifecycle_transition   = "autoscaling:EC2_INSTANCE_LAUNCHING"
+  default_result         = "CONTINUE"
+  heartbeat_timeout      = 120
 
-#   notification_target_arn = "arn:aws:sns:us-east-1:911167901101:asg_launch"
-#   role_arn                = aws_iam_role.example_lifecycle_role.arn
-# }
+  notification_target_arn = "arn:aws:sns:us-east-1:911167901101:asg_launch"
+  role_arn                = aws_iam_role.example_lifecycle_role.arn
+}
 
 
-# output "enis" {
-#   description = "The list of subnet IDs"
-#   value       = aws_network_interface.data_eni
-# }
+output "enis" {
+  description = "The list of subnet IDs"
+  value       = aws_network_interface.data_eni
+}
