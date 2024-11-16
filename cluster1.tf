e127357e975552d4961206b86b894ce062edb5ed
Ravipraksh Pochiraju <raviprakash244@gmail.com>
2024-11-16 11:52:24 +0530

    Testing terraform code

--- cluster1.tf
+++ cluster1.tf
@@ -1,4 +1,3 @@
-# Setup AWS provider with remote backend
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
@@ -9,52 +8,55 @@ terraform {
   }
 }
 
-# Define the subnet names list as a variable
 variable "subnet_name_list" {
   type    = list(string)
   default = ["private-subnet-1", "private-subnet-2", "private-subnet-3"]
 }
 
-# Retrieve subnets with the specified tags
 data "aws_subnet" "subnets" { 
-  count = 3
+  for_each = toset(var.subnet_name_list)  
   filter { 
     name   = "tag:Name"
-    values = ["*${var.subnet_name_list[(count.index % 3)]}*"]
+    values = ["*${each.value}*"]
   }
 }
 
-# Local variable to hold subnet IDs
 locals {
   subnet_ids = [for subnet in data.aws_subnet.subnets : subnet.id]
 }
 
+resource "aws_network_interface" "eni" {
+  for_each = toset(local.subnet_ids) 
+
+  subnet_id       = each.value  
+  security_groups = ["sg-057749862a8753300"]
+
+  tags = {
+    Name = "eni-instance-${each.key + 1}"
+  }
+}
 
 
-# Launch Template for EC2 instances
 resource "aws_launch_template" "example" {
-  name          = "couchbase-data-launch-template"
-  image_id      = "ami-030c239b5d3296394"  # Replace with your AMI ID
-  instance_type = "t2.micro"                # Replace with your instance type
-
-  key_name      = "terminal"               # Replace with your SSH key
+  for_each      = toset(local.subnet_ids) 
+  name          = "couchbase-data-launch-template-${each.value}"
+  image_id      = "ami-030c239b5d3296394"  
+  instance_type = "t2.micro"                
+  key_name      = "terminal"                
 
   network_interfaces {
     associate_public_ip_address = false
     security_groups             = ["sg-057749862a8753300"]
-    subnet_id                   = local.subnet_ids[count.index] 
-    device_index         = 0
+    network_interface_id        = aws_network_interface.eni[each.key].id  
+    device_index                = 0
   }
 
   user_data = base64encode(<<EOF
 #!/bin/bash
-
 yum install -y aws-cli
 INSTANCE_ID=$$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 REGION="us-east-1"  # Correct region identifier
 TAG_NAME="data-$${INSTANCE_ID}"
-
-# Add tags to the instance
 aws ec2 create-tags \
   --resources "$$INSTANCE_ID" \
   --tags Key=UniqueName,Value="$$TAG_NAME" \
@@ -65,15 +67,15 @@ EOF
 
 
 
-# Auto Scaling Group resource
+
 resource "aws_autoscaling_group" "couchbase_data" {
   desired_capacity     = 3
   max_size             = 3
   min_size             = 3
-  vpc_zone_identifier  = local.subnet_ids
+  vpc_zone_identifier  = local.subnet_ids  
 
   launch_template {
-    id      = aws_launch_template.example.id
+    id      = aws_launch_template.example[local.subnet_ids[0]].id  
     version = "$Latest"
   }
 
@@ -98,7 +100,7 @@ resource "aws_autoscaling_group" "couchbase_data" {
   health_check_grace_period = 60
 }
 
-# IAM Role for Auto Scaling Lifecycle Hook to access SNS
+
 resource "aws_iam_role" "example_lifecycle_role" {
   name = "example-lifecycle-role"
   assume_role_policy = <<EOF
@@ -118,7 +120,6 @@ resource "aws_iam_role" "example_lifecycle_role" {
 EOF
 }
 
-# IAM Role Policy to allow SNS publishing
 resource "aws_iam_role_policy" "example_lifecycle_policy" {
   role = aws_iam_role.example_lifecycle_role.name
   policy = <<EOF
@@ -135,7 +136,6 @@ resource "aws_iam_role_policy" "example_lifecycle_policy" {
 EOF
 }
 
-# Auto Scaling Lifecycle Hook to capture instance launch event
 resource "aws_autoscaling_lifecycle_hook" "example_hook" {
   autoscaling_group_name = aws_autoscaling_group.couchbase_data.name
   name                 = "example-lifecycle-hook"
@@ -147,7 +147,6 @@ resource "aws_autoscaling_lifecycle_hook" "example_hook" {
   role_arn                = aws_iam_role.example_lifecycle_role.arn
 }
 
-# Output to display subnet IDs
 output "subnet_ids" {
   description = "The list of subnet IDs"
   value       = [for subnet in data.aws_subnet.subnets : subnet.id]
