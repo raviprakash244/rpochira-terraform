48a15eefe30b577ec6d5d4476f23d8f5da178263
Ravipraksh Pochiraju <raviprakash244@gmail.com>
2024-11-16 21:00:56 +0530

    Testing terraform code

--- cluster1.tf
+++ cluster1.tf
@@ -56,110 +56,110 @@ resource "aws_network_interface" "data_eni" {
   }
 }
 
-# resource "aws_launch_template" "couchbase_data" {
-#   name          = "couchbase-data-launch-template"
-#   image_id      = var.ami_id
-#   instance_type = var.instance_type
-#   key_name      = "terminal"
+resource "aws_launch_template" "couchbase_data" {
+  name          = "couchbase-data-launch-template"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+  key_name      = "terminal"
 
-#   network_interfaces {
-#     associate_public_ip_address = false
-#     security_groups             = [var.security_group]
-#   }
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [var.security_group]
+  }
 
-#   user_data = base64encode(<<EOF
-# #!/bin/bash
-# yum install -y aws-cli
-# INSTANCE_ID=$$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-# REGION="us-east-1"  # Correct region identifier
-# TAG_NAME="data-$${INSTANCE_ID}"
-# aws ec2 create-tags \
-#   --resources "$$INSTANCE_ID" \
-#   --tags Key=UniqueName,Value="$$TAG_NAME" \
-#   --region "$$REGION" >> /tmp/userdata.log 2>&1
-# EOF
-#   )
-# }
+  user_data = base64encode(<<EOF
+#!/bin/bash
+yum install -y aws-cli
+INSTANCE_ID=$$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+REGION="us-east-1"  # Correct region identifier
+TAG_NAME="data-$${INSTANCE_ID}"
+aws ec2 create-tags \
+  --resources "$$INSTANCE_ID" \
+  --tags Key=UniqueName,Value="$$TAG_NAME" \
+  --region "$$REGION" >> /tmp/userdata.log 2>&1
+EOF
+  )
+}
 
-# resource "aws_autoscaling_group" "couchbase_data" {
-#   desired_capacity    = var.instance_count
-#   max_size            = var.instance_count
-#   min_size            = var.instance_count
-#   vpc_zone_identifier = local.subnet_ids
+resource "aws_autoscaling_group" "couchbase_data" {
+  desired_capacity    = var.instance_count
+  max_size            = var.instance_count
+  min_size            = var.instance_count
+  vpc_zone_identifier = local.subnet_ids
 
-#   launch_template {
-#     id      = aws_launch_template.couchbase_data.id 
-#     version = "$Latest"
-#   }
+  launch_template {
+    id      = aws_launch_template.couchbase_data.id 
+    version = "$Latest"
+  }
 
-#   tag {
-#     key                 = "Name"
-#     value               = "asg-cb-data"
-#     propagate_at_launch = true
-#   }
+  tag {
+    key                 = "Name"
+    value               = "asg-cb-data"
+    propagate_at_launch = true
+  }
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
+  lifecycle {
+    create_before_destroy = true
+  }
 
-#   instance_refresh {
-#     strategy = "Rolling"
-#     preferences {
-#       min_healthy_percentage = 100
-#     }
-#   }
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 100
+    }
+  }
 
-#   health_check_type         = "EC2"
-#   health_check_grace_period = 60
-# }
+  health_check_type         = "EC2"
+  health_check_grace_period = 60
+}
 
-# resource "aws_iam_role" "example_lifecycle_role" {
-#   name = "example-lifecycle-role"
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "autoscaling.amazonaws.com"
-#       },
-#       "Effect": "Allow",
-#       "Sid": ""
-#     }
-#   ]
-# }
-# EOF
-# }
+resource "aws_iam_role" "example_lifecycle_role" {
+  name = "example-lifecycle-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "autoscaling.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
 
-# resource "aws_iam_role_policy" "example_lifecycle_policy" {
-#   role = aws_iam_role.example_lifecycle_role.name
-#   policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Action": "sns:Publish",
-#       "Resource": "arn:aws:sns:us-east-1:911167901101:asg_launch"
-#     }
-#   ]
-# }
-# EOF
-# }
+resource "aws_iam_role_policy" "example_lifecycle_policy" {
+  role = aws_iam_role.example_lifecycle_role.name
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sns:Publish",
+      "Resource": "arn:aws:sns:us-east-1:911167901101:asg_launch"
+    }
+  ]
+}
+EOF
+}
 
-# resource "aws_autoscaling_lifecycle_hook" "example_hook" {
-#   for_each = toset(local.subnet_ids)  # Iterate over subnets or ASGs
+resource "aws_autoscaling_lifecycle_hook" "example_hook" {
+  for_each = toset(local.subnet_ids)  # Iterate over subnets or ASGs
 
-#   autoscaling_group_name  = aws_autoscaling_group.couchbase_data.name  
-#   name                   = "asg-cbdata-lifecycle-hook"
-#   lifecycle_transition   = "autoscaling:EC2_INSTANCE_LAUNCHING"
-#   default_result         = "CONTINUE"
-#   heartbeat_timeout      = 120
+  autoscaling_group_name  = aws_autoscaling_group.couchbase_data.name  
+  name                   = "asg-cbdata-lifecycle-hook"
+  lifecycle_transition   = "autoscaling:EC2_INSTANCE_LAUNCHING"
+  default_result         = "CONTINUE"
+  heartbeat_timeout      = 120
 
-#   notification_target_arn = "arn:aws:sns:us-east-1:911167901101:asg_launch"
-#   role_arn                = aws_iam_role.example_lifecycle_role.arn
-# }
+  notification_target_arn = "arn:aws:sns:us-east-1:911167901101:asg_launch"
+  role_arn                = aws_iam_role.example_lifecycle_role.arn
+}
 
 
 output "enis" {
